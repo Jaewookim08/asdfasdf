0407310c9c56c8c8c374fec9241e169ff93e18ff
Taeyoon <cloudloverty@kaist.ac.kr>
2019-11-10 02:28:33 +0900

    add jump feature, change direction feature

---
+++ Assets/Scripts/Netrunner/EnemyAI/EnemyGroundCheck.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroundCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Tilemap")
+        {
+            EnemyMovement em = gameObject.transform.parent.GetComponent<EnemyMovement>();
+            em.onGround = true;
+            Debug.Log("on Ground");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Tilemap")
+        {
+            EnemyMovement em = gameObject.transform.parent.GetComponent<EnemyMovement>();
+            em.onGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Tilemap")
+        {
+            EnemyMovement em = gameObject.transform.parent.GetComponent<EnemyMovement>();
+            em.onGround = false;
+            Debug.Log("get away from ground");
+        }
+    }
+}
--- Assets/Scripts/Netrunner/EnemyAI/EnemyMovement.cs
+++ Assets/Scripts/Netrunner/EnemyAI/EnemyMovement.cs
@@ -7,14 +7,18 @@ using UnityEngine;
 /// Enemy의 하위 오브젝트인 Dest를 순서대로 정찰합니다.
 /// Dest 큐브를 복사해서 enemy가 움직일 경로를 설정할 수 있습니다.
 /// 모든 Dest를 정찰하면, 역순으로 다시 정찰합니다.
-/// 현재는 같은 높이에, 장애물이 없는 경우에만 패트롤 AI가 작동하는데
-/// 후에 점프 기능을 넣을 예정입니다.
+/// 간단한 점프 기능이 있습니다. 아직은 좀 버벅거립니다.
+/// 모든 움직이는 적은 기본적으로 생성 시 오른쪽을 바라보게 설정되어 있습니다.
 /// </summary>
 
 
 public class EnemyMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+    public bool facingWall;
+    public bool onGround;
+    public bool right;
 
     Vector3 originalPos;
     List<Vector3> destPos;
@@ -22,14 +26,16 @@ public class EnemyMovement : MonoBehaviour
 
     int iterator;
     bool incr;
+    Rigidbody2D rigid;
 
 
     // Start is called before the first frame update
     void Start()
     {
         originalPos = transform.position;
-
         destPos = new List<Vector3>();
+        destPos.Add(originalPos);
+
         Transform PatrolPath = transform.Find("PatrolPath");
         for (int i = 0; i < PatrolPath.childCount; i++)
         {
@@ -42,6 +48,8 @@ public class EnemyMovement : MonoBehaviour
         iterator = 0;
         incr = true;
 
+        right = true;
+        rigid = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -58,10 +66,21 @@ public class EnemyMovement : MonoBehaviour
             updateIterator();
             currentDest = destPos[iterator];
             Debug.Log("Current Dest: " + currentDest);
+            bool previousDirection = right;
+            right = currentDest.x > transform.position.x ? true : false;
+            if (previousDirection != right)
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+        }
+
+        if (facingWall && onGround)
+        {
+            rigid.AddForce(new Vector2(0f,1) * jumpForce, ForceMode2D.Impulse);
+            Debug.Log("jump");
         }
 
         float speed = moveSpeed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, new Vector3(destPos[iterator].x, originalPos.y, originalPos.z), speed);
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(destPos[iterator].x, transform.position.y, originalPos.z), speed);
     }
 
     void updateIterator()
