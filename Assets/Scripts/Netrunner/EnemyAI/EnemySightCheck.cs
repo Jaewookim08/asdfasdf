32873a981751d048d3e52d5d940ebd021ff85296
Taeyoon <cloudloverty@kaist.ac.kr>
2019-11-14 16:43:32 +0900

    enemy sight angle implment

--- Assets/Scripts/Netrunner/EnemyAI/EnemySightCheck.cs
+++ Assets/Scripts/Netrunner/EnemyAI/EnemySightCheck.cs
@@ -6,16 +6,19 @@ public class EnemySightCheck : MonoBehaviour
 {
     public float sightRadius;
     public float sightAngle;
+    public GameObject parent;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale = new Vector3(0.566f * sightRadius, transform.localScale.y, transform.localScale.z);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        DrawView();
         EnemySight();
     }
 
@@ -26,14 +29,39 @@ public class EnemySightCheck : MonoBehaviour
         while (i < hitColliders.Length)
         {
             GameObject foundObject = hitColliders[i].gameObject;
-            if (foundObject.tag == "Player")
+            if (inSightAngle(foundObject) && foundObject.tag == "Player")
             {
-                Debug.Log("player detected");
+                Debug.DrawLine(transform.position, foundObject.transform.position, Color.red);
             }
             i++;
         }
     }
 
+    bool inSightAngle(GameObject target)
+    {
+        Vector3 targetPos = target.transform.position;
+        Vector3 toTarget = (targetPos - transform.position).normalized;
+        Vector3 toForward = parent.GetComponent<EnemyMovement>().right ? Vector3.right : Vector3.left;
+
+        if (Vector3.Dot(toForward, toTarget) > Mathf.Cos((sightAngle / 2) * Mathf.Deg2Rad))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void DrawView()
+    {
+        Vector3 upEnd = parent.GetComponent<EnemyMovement>().right
+            ? new Vector3(Mathf.Cos((sightAngle / 2) * Mathf.Deg2Rad), Mathf.Sin((sightAngle / 2) * Mathf.Deg2Rad), 0).normalized
+            : new Vector3(-Mathf.Cos((sightAngle / 2) * Mathf.Deg2Rad), Mathf.Sin((sightAngle / 2) * Mathf.Deg2Rad), 0).normalized;
+        Vector3 downEnd = parent.GetComponent<EnemyMovement>().right
+    ? new Vector3(Mathf.Cos((sightAngle / 2) * Mathf.Deg2Rad), -Mathf.Sin((sightAngle / 2) * Mathf.Deg2Rad), 0).normalized
+    : new Vector3(-Mathf.Cos((sightAngle / 2) * Mathf.Deg2Rad), -Mathf.Sin((sightAngle / 2) * Mathf.Deg2Rad), 0).normalized;
+        Debug.DrawLine(transform.position, transform.position + upEnd * sightRadius, Color.blue);
+        Debug.DrawLine(transform.position, transform.position + downEnd * sightRadius, Color.blue);
+    }
 
     void OnDrawGizmosSelected()
     {
