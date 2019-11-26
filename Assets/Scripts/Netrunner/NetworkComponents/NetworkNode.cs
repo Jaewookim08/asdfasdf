390a0ee4eebfe4d0ca2c6933665adfbb61c9442c
AsteriskGameDev <dylee063@gmail.com>
2019-11-26 23:40:21 +0900

    자판기, 네트워크 노스 사용 제한 추가중 버그있습니다.

--- Assets/Scripts/Netrunner/NetworkComponents/NetworkNode.cs
+++ Assets/Scripts/Netrunner/NetworkComponents/NetworkNode.cs
@@ -10,6 +10,7 @@ namespace Netrunner.Network
     {
         public PlayerContainedTracker tracker;
         const float PacketVelocity = 20f;
+        public int MaxUse = -1;
         
         [HideInInspector]
         public bool[] Player = new bool[3];
@@ -29,23 +30,24 @@ namespace Netrunner.Network
         {
             if (Player[1] && Movable[1])
             {
-                if (Connections[0] != null && GameInput.GetKeyDown(1, GameInput.Key.Right)) MovePlayer(1, 0);
-                else if (Connections[1] != null && GameInput.GetKeyDown(1, GameInput.Key.Up)) MovePlayer(1, 1);
-                else if (Connections[2] != null && GameInput.GetKeyDown(1, GameInput.Key.Left)) MovePlayer(1, 2);
-                else if (Connections[3] != null && GameInput.GetKeyDown(1, GameInput.Key.Down)) MovePlayer(1, 3);
+                if (Connections[0] != null && Connections[0].MoveAvailable() && GameInput.GetKeyDown(1, GameInput.Key.Right)) MovePlayer(1, 0);
+                else if (Connections[1] != null && Connections[1].MoveAvailable() && GameInput.GetKeyDown(1, GameInput.Key.Up)) MovePlayer(1, 1);
+                else if (Connections[2] != null && Connections[2].MoveAvailable() && GameInput.GetKeyDown(1, GameInput.Key.Left)) MovePlayer(1, 2);
+                else if (Connections[3] != null && Connections[3].MoveAvailable() && GameInput.GetKeyDown(1, GameInput.Key.Down)) MovePlayer(1, 3);
             }
             if (Player[2] && Movable[2])
             {
-                if (Connections[0] != null && GameInput.GetKeyDown(2, GameInput.Key.Right)) MovePlayer(2, 0);
-                else if (Connections[1] != null && GameInput.GetKeyDown(2, GameInput.Key.Up)) MovePlayer(2, 1);
-                else if (Connections[2] != null && GameInput.GetKeyDown(2, GameInput.Key.Left)) MovePlayer(2, 2);
-                else if (Connections[3] != null && GameInput.GetKeyDown(2, GameInput.Key.Down)) MovePlayer(2, 3);
+                if (Connections[0] != null && Connections[0].MoveAvailable() && GameInput.GetKeyDown(2, GameInput.Key.Right)) MovePlayer(2, 0);
+                else if (Connections[1] != null && Connections[1].MoveAvailable() && GameInput.GetKeyDown(2, GameInput.Key.Up)) MovePlayer(2, 1);
+                else if (Connections[2] != null && Connections[2].MoveAvailable() && GameInput.GetKeyDown(2, GameInput.Key.Left)) MovePlayer(2, 2);
+                else if (Connections[3] != null && Connections[3].MoveAvailable() && GameInput.GetKeyDown(2, GameInput.Key.Down)) MovePlayer(2, 3);
             }
         }
 
         protected virtual void MovePlayer(int player, int direction)
         {
             PlayerPacket.Instances[player].MoveTo(transform.position, Connections[direction], Connections[direction].transform, PacketVelocity);
+            Connections[direction].MoveDeclare(player);
             MoveOut(player);
         }
 
@@ -56,6 +58,17 @@ namespace Netrunner.Network
             if (!(Player[1] || Player[2]))
                 foreach (NodeAction action in Actions)
                     action.enabled = false;
+        }
+
+        public bool MoveAvailable()
+        {
+            if (MaxUse == -1 || MaxUse > 1) return true;
+            else return false;
+        }
+
+        public void MoveDeclare(int player)
+        {
+            if (MaxUse > 1) MaxUse--;
         }
 
         public virtual void MoveIn(int player)
