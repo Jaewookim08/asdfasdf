14eb7b20cf165522fe895719cc1321f770598e53
AsteriskGameDev <dylee063@gmail.com>
2019-11-28 02:03:51 +0900

    Revert "버그"
    This reverts commit 13e4a78ae396bfad0cc7bd60958961c73be734eb.

--- Assets/Scripts/Netrunner/ModuleComponents/DispenseAction.cs
+++ Assets/Scripts/Netrunner/ModuleComponents/DispenseAction.cs
@@ -5,17 +5,15 @@ using UnityEngine.Tilemaps;
 namespace Netrunner.ModuleComponents {
     public class DispenseAction: ModuleAction {
         public GameObject CanPrefab;
-        public int DrinkCount = 2;
 
         private void Update() {
-            if (GameInput.GetKeyDown(player, ActionKey) && DrinkCount > 0)
+            if (GameInput.GetKeyDown(player, ActionKey))
             {
                 float dir = 1;
                 GameObject g = Instantiate(CanPrefab);
                 Rigidbody2D r2d = g.GetComponent<Rigidbody2D>();
                 g.transform.position = transform.position + new Vector3(dir * 1, -.5f, 0);
                 r2d.velocity = new Vector2(5f * dir, 1f);
-                DrinkCount--;
             }
         }
         
