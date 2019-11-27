36fb21d3b41f0ee157cc56234a830ce65c9b8bad
AsteriskGameDev <dylee063@gmail.com>
2019-11-28 02:49:45 +0900

    자판기 캔 개수 제한, 닿으면 죽는 영역

---
+++ Assets/Scripts/Netrunner/Objects/DeathVolume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Netrunner.ModuleComponents;
+
+namespace Netrunner.Objects
+{
+    public class DeathVolume : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            ModuleManager mm = collision.GetComponent<ModuleManager>();
+            if (mm != null)
+            {
+                SpriteRenderer sr = collision.GetComponentInChildren<SpriteRenderer>(false);
+                if (sr != null) sr.color = sr.color * 0.6f;
+                mm.ModuleDestroy();
+            }
+        }
+    }
+
+}
